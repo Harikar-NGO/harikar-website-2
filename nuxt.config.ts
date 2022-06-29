cddb024c19dca88aa9440b46aeecee76afce9b47
ronniedroid <ronni.n.yousif@gmail.com>
2022-06-29 12:53:20 +0300

    added meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,12 @@ import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  components: true,
-  css: ["~/assets/style.css"],
-  modules: ["@nuxtjs/color-mode"],
-  head: {
-    title: "Harikar NGO website",
-    htmlAttrs: {
-      lang: "en",
-    },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-
-      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-      {
-        hid: "description",
-        name: "description",
-        content: "Harikar NGO website",
-      },
-    ],
-  },
+    components: true,
+    css: ["~/assets/style.css"],
+    modules: ["@nuxtjs/color-mode"],
+    meta: {
+        title: "Harikar NGO website",
+        charset: "utf-8",
+        viewport: "width=device-width, initial-scale=1"
+        }
 });
